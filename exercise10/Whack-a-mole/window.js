b61d98a977762c71e6ae51d90bc335b3d371369d
Hardik_Gupta <63852281+hardikgupta08@users.noreply.github.com>
2023-03-16 17:05:56 +0530

    Add files via upload

---
+++ exercise10/Whack-a-mole/window.js
@@ -0,0 +1,7 @@
+function whack_a_mole(){
+    var start=0;
+    while(1){
+        window.open('Whack-a-mole.html',start,"noscroll");
+        start++;
+    }
+}
---
+++ exercise10/endless alert/alert.js
@@ -0,0 +1,3 @@
+while(1){
+    alert("Your PC has been attacked!!!!!!");
+}
